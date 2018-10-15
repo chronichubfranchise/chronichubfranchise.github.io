24a052d045b652d30c2c635d35694a043ca7dec8
Heesp <yaroslav.kostiukov@gmail.com>
2018-10-14 22:08:26 -0700

    final changes

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	$(".fullscreen").css("height", window_height)
 	$(".fitscreen").css("height", fitscreen);
 
-     
+
      // -------   Active Mobile Menu-----//
 
     $(".menu-bar").on('click', function(e){
@@ -21,7 +21,7 @@ $(document).ready(function(){
         $("span", this).toggleClass("lnr-menu lnr-cross");
         $(".main-menu").addClass('mobile-menu');
     });
-     
+
     $('select').niceSelect();
     $('.img-pop-up').magnificPopup({
         type: 'image',
@@ -56,14 +56,49 @@ $(document).ready(function(){
               $('html, body').animate({
                 scrollTop: $(hash).offset().top - (-10)
             }, 600, function(){
-             
+
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 
     $(document).ready(function() {
         $('#mc_embed_signup').find('form').ajaxChimp();
-    });      
+    });
+
+ });
+
+ $(function(){
+
+   // Add smooth scrolling to all links
+   $("a").on('click', function(event) {
+
+     // Make sure this.hash has a value before overriding default behavior
+     if (this.hash !== "") {
+       // Prevent default anchor click behavior
+       event.preventDefault();
+
+       // Store hash
+       var hash = this.hash;
+
+       // Using jQuery's animate() method to add smooth page scroll
+       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 1500, function(){
+
+         // Add hash (#) to URL when done scrolling (default click behavior)
+         window.location.hash = hash;
+       });
+     } // End if
+   });
+
+   $(window).on("scroll", function(){
+     if ($(window).scrollTop()>1000) {
+       $(".btn-scroll-up").addClass("visible")
+     }else {
+       $(".btn-scroll-up").removeClass("visible")
+     }
+   })
 
  });
